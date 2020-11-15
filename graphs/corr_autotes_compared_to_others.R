60b5ee7444d864664d87ea72e8978a464313e57f
Zolano <j.houvenaeghel@laposte.net>
2020-11-15 16:38:13 +0100

    refacto - add 1st step results as png

--- graphs/corr_autotes_compared_to_others.R
+++ graphs/corr_autotes_compared_to_others.R
@@ -17,6 +17,8 @@ library(cowplot)
 
 setDT(ReccueilR)
 
+source("utils/fn_correlation_scores_autotest.R")
+
 #filter on useful columns non-empty
 mydata = ReccueilR[
   !is.na(Oxford)
@@ -26,181 +28,28 @@ mydata = ReccueilR[
   &!is.na(Autotest)
 ]
 
-#SPEARMAN CORRELATION CALCULATION
-
-  # calcul de la correlation de spearman entre Autotest (x) et Oxford (y)
+#REPRESENTATION OF CORRELATIONS between AUTOTEST and other scores
   
-  cor.test(formula=~Oxford+Autotest,data=mydata,method = "spearman",alternative = "greater")# -> (rho=0.8547031, p=1e-07)
-  corOxford = cor(x=mydata$Autotest, y=mydata$Oxford, method="spearman") # -> 0.8547031
+plot_all_corr = generate_histogram_autotest_corr_with_other_scores(mydata)
+plot_all_corr
   
-  # calcul de la correlation de spearman entre Autotest (x) et Womac (y)
-  cor.test(formula=~Womac+Autotest,data=mydata,method = "spearman",alternative = "greater")# -> (rho=0.931137, p=5e-011)
-  corWomac = cor(x=mydata$Autotest, y=mydata$Womac, method="spearman") # -> 0.931137
-  
-  # calcul de la correlation de spearman entre Autotest (x) et Harris Hip Score (y)
-  cor.test(formula=~HarrisHS+Autotest,data=mydata,method = "spearman",alternative = "less")# -> (rho=-0.866056, p=4e-08)
-  corHarris = cor(x=mydata$Autotest, y=mydata$HarrisHS, method="spearman") # -> -0.866056
-  
-  # calcul de la correlation de spearman entre Autotest (x) et PMA  (y)
-  
-  cor.test(formula=~PMA+Autotest,data=mydata,method = "spearman", alternative="less")# -> (rho=-0.8928448, p=1e-08)
-  corPMA = cor(x=mydata$Autotest, y=mydata$PMA, method="spearman") # -> 0.8928448
-
-#REPRESENTATION OF CORRELATIONS
-  
-  #preparing a dataframe to store the result of previous stats calculations 
-  N <- 1e4
-  testcors <- data.frame(
-                  TestName=character(),
-                  Rho=integer(),
-                  stringsAsFactors=FALSE
-                ) 
-  
-  #storing results in dataframe
-  testcors[1, ] <- list("Oxford",abs(corOxford))
-  testcors[2, ] <- list("Womac", abs(corWomac))
-  testcors[3, ] <- list("Harris", abs(corHarris))
-  testcors[4, ] <- list("PMA", abs(corPMA))
-  
-  #plotting dataframe as histogram
-  p0 = testcors %>%
-    ggplot(aes(x=TestName, y=Rho, label=round(Rho, 3))) +
-    geom_bar(
-      aes(fill = Rho),
-      stat='identity',
-      orientation="x",
-      colour="white"
-    ) +
-    geom_text(size = 4, position = position_stack(vjust = 0.9), color="white") +
-    labs (
-      title= "Corrélation de l'auto-test aux autres tests",
-      x="Tests Connus",
-      y="Corrélation de Spearman"
-    ) +
-    theme(
-      legend.position = "right",
-      axis.title.y = element_text(colour= "red")
-    )
-p0
-  
-# Display 4 linear regression lines on 1 plot
-p0bis = mydata %>%
-  ggplot( aes(x=Autotest, y=Oxford)) +
-  geom_smooth(
-    color="darkgreen",
-    fill="darkgreen",
-    aes(x=Autotest, y=Oxford),
-    method="lm", 
-    se=FALSE, 
-    fullrange=FALSE, 
-    level=0.95
-  ) +   
-  geom_smooth(
-    color="darkblue",
-    fill="darkblue",
-    aes(x=Autotest, y=Womac),
-    method="lm", 
-    se=FALSE, 
-    fullrange=FALSE, 
-    level=0.95
-  ) +
-  geom_smooth(
-    color="darkred",
-    fill="darkred",
-    aes(x=Autotest, y=(-HarrisHS)),
-    method="lm", 
-    se=FALSE, 
-    fullrange=FALSE, 
-    level=0.95
-  ) +
-  geom_smooth(
-    color="darkorange",
-    fill="darkorange",
-    aes(x=Autotest, y=(-PMA)),
-    method="lm", 
-    se=FALSE, 
-    fullrange=FALSE, 
-    level=0.95
-  )
-p0bis
+plot_linreg_all_scores = generate_4_regression_lines_same_graph(mydata)
+plot_linreg_all_scores
 
 # DISPLAY SCATTERING
 
 
 # Autotest x Oxford
-p1 = mydata %>%
-ggplot( aes(x=Autotest, y=Oxford)) +
-  geom_point(
-    size=2, 
-    shape=20,
-    color="darkgreen"
-  ) +
-  geom_smooth(
-    color="darkgreen",
-    fill="darkgreen",
-    aes(x=Autotest, y=Oxford),
-    method="lm", 
-    se=TRUE, 
-    fullrange=FALSE, 
-    level=0.995
-  )
-
+pOxford = generate_scattering_autotest_oxford(mydata)
 
 # Autotest x Womac
-p2 = mydata %>%
-ggplot( aes(x=Autotest, y=Womac)) +
-  geom_point(
-    size=2, 
-    shape=11,
-    color="darkblue"
-  ) +
-  geom_smooth(
-    color="darkblue",
-    fill="darkblue",
-    aes(x=Autotest, y=Womac),
-    method="lm", 
-    se=TRUE, 
-    fullrange=FALSE, 
-    level=0.995
-  )
-
-
+pWomac = generate_scattering_autotest_womac(mydata)
 
 # Autotest x Harris
-p3 = mydata %>%
-ggplot( aes(x=Autotest, y=-HarrisHS)) +
-  geom_point(
-    size=2, 
-    shape=17,
-    color="darkred"
-  ) +
-  geom_smooth(
-    color="darkred",
-    fill="darkred",
-    aes(x=Autotest, y=-HarrisHS),
-    method="lm", 
-    se=TRUE, 
-    fullrange=FALSE, 
-    level=0.995
-  )
-p3
+pHarris = generate_scattering_autotest_harris(mydata)
 
-p4 = 
-  mydata %>%
-  ggplot( aes(x=Autotest, y=-PMA)) +
-  geom_point(
-    size=2, 
-    shape=17,
-    color="orange"
-  ) +
-  geom_smooth(
-    color="darkorange",
-    fill="darkorange",
-    aes(x=Autotest, y=-PMA),
-    method="lm", 
-    se=TRUE, 
-    fullrange=FALSE, 
-    level=0.995
-  )
-p4
+# Autotest x PMA 
+pPMA =  generate_scattering_autotest_pma(mydata)
+
+plot_grid(pOxford, pWomac, pHarris, pPMA, labels=c("Oxford", "Womac", "Harris", "PMA"),ncol = 2, nrow = 2)
 
---
+++ utils/fn_correlation_scores_autotest.R
@@ -0,0 +1,203 @@
+#functions for correlations between autotest and other tests
+
+#scattering for Oxford x Autotest
+generate_scattering_autotest_oxford <- function(dataset){
+  
+  plot = dataset %>%
+    ggplot( aes(x=Autotest, y=Oxford)) +
+    geom_point(
+      size=2, 
+      shape=20,
+      color="darkgreen"
+    ) +
+    geom_smooth(
+      color="darkgreen",
+      fill="darkgreen",
+      aes(x=Autotest, y=Oxford),
+      method="lm", 
+      se=TRUE, 
+      fullrange=FALSE, 
+      level=0.995
+    )
+  
+  return(plot)
+}
+
+#scattering for Womac x Autotest
+generate_scattering_autotest_womac <- function(dataset){
+  plot = dataset %>%
+    ggplot( aes(x=Autotest, y=Womac)) +
+    geom_point(
+      size=2, 
+      shape=22,
+      color="darkblue"
+    ) +
+    geom_smooth(
+      color="darkblue",
+      fill="darkblue",
+      aes(x=Autotest, y=Womac),
+      method="lm", 
+      se=TRUE, 
+      fullrange=FALSE, 
+      level=0.995
+    )
+  return(plot)
+} 
+
+#scattering for Harris HS x Autotest
+generate_scattering_autotest_harris <- function(dataset){
+  plot = dataset %>%
+    ggplot( aes(x=Autotest, y=-HarrisHS)) +
+    geom_point(
+      size=2, 
+      shape=17,
+      color="darkred"
+    ) +
+    geom_smooth(
+      color="darkred",
+      fill="darkred",
+      aes(x=Autotest, y=-HarrisHS),
+      method="lm", 
+      se=TRUE, 
+      fullrange=FALSE, 
+      level=0.995
+    )
+  return(plot)
+} 
+
+#scattering for PMA x Autotest
+generate_scattering_autotest_pma <- function(dataset){
+  plot = dataset %>%
+    ggplot( aes(x=Autotest, y=-PMA)) +
+    geom_point(
+      size=2, 
+      shape=17,
+      color="orange"
+    ) +
+    geom_smooth(
+      color="darkorange",
+      fill="darkorange",
+      aes(x=Autotest, y=-PMA),
+      method="lm", 
+      se=TRUE, 
+      fullrange=FALSE, 
+      level=0.995
+    )
+  return(plot)
+} 
+
+
+#graph with linear regression lines for each score
+generate_4_regression_lines_same_graph <- function(dataset){
+  # Display 4 linear regression lines on 1 plot
+  plot = dataset %>%
+    ggplot( aes(x=Autotest, y=Oxford)) +
+    geom_smooth(
+      color="darkgreen",
+      fill="darkgreen",
+      aes(x=Autotest, y=Oxford),
+      method="lm", 
+      se=FALSE, 
+      fullrange=FALSE, 
+      level=0.95
+    ) +   
+    geom_smooth(
+      color="darkblue",
+      fill="darkblue",
+      aes(x=Autotest, y=Womac),
+      method="lm", 
+      se=FALSE, 
+      fullrange=FALSE, 
+      level=0.95
+    ) +
+    geom_smooth(
+      color="darkred",
+      fill="darkred",
+      aes(x=Autotest, y=(-HarrisHS)),
+      method="lm", 
+      se=FALSE, 
+      fullrange=FALSE, 
+      level=0.95
+    ) +
+    geom_smooth(
+      color="darkorange",
+      fill="darkorange",
+      aes(x=Autotest, y=(-PMA)),
+      method="lm", 
+      se=FALSE, 
+      fullrange=FALSE, 
+      level=0.95
+    )
+  return(plot)
+} 
+
+generate_histogram_autotest_corr_with_other_scores <- function(dataset){
+  
+  #calculate correlations
+  corOxford = spearman_autotest_oxford(mydata) # -> 0.8547031
+  corWomac = spearman_autotest_womac(mydata) # -> 0.931137
+  corHarris = spearman_autotest_harris(mydata) # -> -0.866056
+  corPMA = spearman_autotest_pma(mydata) # -> 0.8928448
+  
+  #preparing data
+  N <- 1e4
+  testcors <- data.frame(
+    TestName=character(),
+    Rho=integer(),
+    stringsAsFactors=FALSE
+  ) 
+  
+  #storing results in dataframe
+  testcors[1, ] <- list("Oxford",abs(corOxford))
+  testcors[2, ] <- list("Womac", abs(corWomac))
+  testcors[3, ] <- list("Harris", abs(corHarris))
+  testcors[4, ] <- list("PMA", abs(corPMA))
+  
+  #plotting dataframe as histogram
+  plot = testcors %>%
+    ggplot(aes(x=TestName, y=Rho, label=round(Rho, 3))) +
+    geom_bar(
+      aes(fill = Rho),
+      stat='identity',
+      orientation="x",
+      colour="white"
+    ) +
+    geom_text(size = 4, position = position_stack(vjust = 0.9), color="white") +
+    labs (
+      title= "Corrélation de l'auto-test aux autres scores",
+      x="Score",
+      y="Corrélation de Spearman"
+    ) +
+    theme(
+      legend.position = "right",
+      axis.title.y = element_text(colour= "red")
+    )
+  
+    return(plot)
+}
+
+# calcul de la correlation de spearman entre Autotest (x) et Oxford (y)
+spearman_autotest_oxford <- function(dataset){
+  # 
+  corOxford = cor(x=dataset$Autotest, y=dataset$Oxford, method="spearman") # -> 0.8547031
+  return(corOxford)
+}
+
+# calcul de la correlation de spearman entre Autotest (x) et Oxford (y)
+spearman_autotest_womac <- function(dataset){
+  corWomac= cor(x=dataset$Autotest, y=dataset$Womac, method="spearman") # -> 0.8547031
+  return(corWomac)
+}
+
+# calcul de la correlation de spearman entre Autotest (x) et Oxford (y)
+spearman_autotest_harris <- function(dataset){
+  corHarris = cor(x=dataset$Autotest, y=dataset$HarrisHS, method="spearman") # -> 0.8547031
+  return(corHarris)
+}
+
+# calcul de la correlation de spearman entre Autotest (x) et Oxford (y)
+spearman_autotest_pma <- function(dataset){
+  corPMA = cor(x=dataset$Autotest, y=dataset$PMA, method="spearman") # -> 0.8547031
+  return(corPMA)
+}
+
