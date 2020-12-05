4d9b09b7d06919be6676f7782343cc76e4b00e2a
jhouvenaeghel <jhouvenaeghel@parkeon.com>
2020-12-05 12:44:22 +0100

    TODO terminated : display % on top of general bar repartition, and remove y legend

--- utils/fn_cas.R
+++ utils/fn_cas.R
@@ -16,6 +16,59 @@ percentage_trauma <- function(dataset, trauma){
 
 }
 
+percentage_fracture <- function(dataset, fracture){
+  
+  total = nrow(dataset)
+  
+  t_fracture <- quote(fracture)
+  occurrences = nrow(dataset[Fracture == eval(t_fracture)])
+  
+  if(occurrences == 0){
+    return(0)
+  }
+  return(round((occurrences / total), 3) *100 )
+  
+}
+
+percentage_abord <- function(dataset, abord){
+  
+  total = nrow(dataset)
+  
+  t_abord <- quote(abord)
+  occurrences = nrow(dataset[Abord == eval(t_abord)])
+  
+  if(occurrences == 0){
+    return(0)
+  }
+  return(round((occurrences / total), 3) *100 )
+}
+
+percentage_xpchir <- function(dataset, xpchir){
+  
+  total = nrow(dataset)
+  
+  t_xpchir <- quote(xpchir)
+  occurrences = nrow(dataset[Chirurgien == eval(t_xpchir)])
+  
+  if(occurrences == 0){
+    return(0)
+  }
+  return(round((occurrences / total), 3) *100 )
+}
+
+percentage_matta <- function(dataset, matta){
+  
+  total = nrow(dataset)
+  
+  t_matta <- quote(matta)
+  occurrences = nrow(dataset[Matta == eval(t_matta)])
+  
+  if(occurrences == 0){
+    return(0)
+  }
+  return(round((occurrences / total), 3) *100 )
+}
+
 #
 # Generate stacked histogram to display the repartition of fracture types according to trauma
 #
@@ -69,17 +122,23 @@ generate_plot_patients_repartition_by_traumatisme <- function(dataset) {
 }
 
 generate_plot_patients_repartition_by_fracture <- function(dataset){
-
-  dataset$FractureX <- as.character(dataset$Fracture)
-  dataset$FractureX[dataset$Fracture == "paroi postérieure"] <- "PP"
-  dataset$FractureX[dataset$Fracture == "transversale et paroi postérieure"] <- "TR-PP"
-  dataset$FractureX[dataset$Fracture == "transversale"] <- "TR"
-  dataset$FractureX[dataset$Fracture == "bi colonne"] <- "BC"
-  dataset$FractureX[dataset$Fracture == "T"] <- "T"
-  dataset$FractureX[dataset$Fracture == "colonne antérieure"] <- "CA"
   
-  dataset$FractureX <- factor(
-    dataset$FractureX, 
+  N <- 1e4
+  plotdataset <- data.frame(
+    Fracture=character(),
+    Percentage=integer(),
+    stringsAsFactors=FALSE
+  )
+  
+  plotdataset[1, ] <- list("PP", percentage_fracture(dataset, "paroi postérieure"))
+  plotdataset[2, ] <- list("TR-PP", percentage_fracture(dataset, "transversale et paroi postérieure"))
+  plotdataset[3, ] <- list("TR", percentage_fracture(dataset, "transversale"))
+  plotdataset[4, ] <- list("BC", percentage_fracture(dataset, "bi colonne"))
+  plotdataset[5, ] <- list("T", percentage_fracture(dataset, "T"))
+  plotdataset[6, ] <- list("CA", percentage_fracture(dataset, "colonne antérieure"))
+  
+  plotdataset$Fracture <- factor(
+    plotdataset$Fracture,
     levels=c(
       "PP",
       "TR-PP",
@@ -90,28 +149,25 @@ generate_plot_patients_repartition_by_fracture <- function(dataset){
     )
   )
   
-  plot = dataset %>%
-    ggplot(aes(x=FractureX)) +
-    geom_bar(
-      stat='count', 
-      orientation="x", 
-      alpha=0.9,
-      colour="white",
+  plot = plotdataset %>%
+    ggplot( aes(x = Fracture, y = Percentage)) +
+    geom_col(
+      alpha=0.6,
       fill=color_generic_stats(),
       width=0.7
     ) +
+    geom_text(aes(label = paste(Percentage, "%")), vjust = -0.3, colour = "black") +
     labs (
       title= "Répartition des patients en fonction du type de fracture",
       y="Nombre de cas",
-      x="Type de Fracture"
+      x="Type de fracture"
     ) +
     default_theming(
       y_title_angle = 90,
       title_size = 14,
       legend_title_size = 12,
-      x_text_angle = 45,
-      x_text_size = 10,
-      x_text_vjust = 0.5
+      y_text_size = 0,
+      y_title_size = 0
     )
   
   return(plot)
@@ -119,10 +175,21 @@ generate_plot_patients_repartition_by_fracture <- function(dataset){
 
 generate_plot_patients_repartition_by_voie_abord <- function(dataset) {
   
-
+  
+  N <- 1e4
+  plotdataset <- data.frame(
+    Abord=character(),
+    Percentage=integer(),
+    stringsAsFactors=FALSE
+  )
+  
+  plotdataset[1, ] <- list("Ilio", percentage_abord(dataset, "Ilio"))
+  plotdataset[2, ] <- list("Kocher", percentage_abord(dataset, "Kocher"))
+  plotdataset[3, ] <- list("Double", percentage_abord(dataset, "Double"))
+  
   #defining order for "Chirurgien"  (/!\ if orientation=x order needs to be reversed)
-  dataset$Abord <- factor(
-    dataset$Abord, 
+  plotdataset$Abord <- factor(
+    plotdataset$Abord, 
     levels=c(
       "Ilio",
       "Kocher", 
@@ -130,28 +197,25 @@ generate_plot_patients_repartition_by_voie_abord <- function(dataset) {
     )
   )
   
-  plot = dataset %>%
-    ggplot(      aes(x=Abord)
-    ) +
-    geom_bar(
-      # position=
-      stat='count', 
-      orientation="x", 
-      alpha=0.9,
-      colour="white",
+  plot = plotdataset %>%
+    ggplot( aes(x = Abord, y = Percentage)) +
+    geom_col(
+      alpha=0.6,
       fill=color_generic_stats(),
       width=0.7
     ) +
-    # scale_y_continuous(labels=scales::percent) +
+    geom_text(aes(label = paste(Percentage, "%")), vjust = -0.3, colour = "black") +
     labs (
       title= "Répartition des patients en fonction de la voie d'abord",
       y="Nombre de cas",
-      x="Abord"
+      x="Voie d'Abord"
     ) +
     default_theming(
       y_title_angle = 90,
       title_size = 14,
-      legend_title_size = 12
+      legend_title_size = 12,
+      y_text_size = 0,
+      y_title_size = 0
     )
   
   return(plot)
@@ -159,10 +223,19 @@ generate_plot_patients_repartition_by_voie_abord <- function(dataset) {
 }
 
 generate_plot_patients_repartition_by_xp_chir <- function(dataset) {
+  N <- 1e4
+  plotdataset <- data.frame(
+    Chirurgien=character(),
+    Percentage=integer(),
+    stringsAsFactors=FALSE
+  )
   
-  #defining order for "Chirurgien"  (/!\ if orientation=x order needs to be reversed)
-  dataset$Chirurgien <- factor(
-    dataset$Chirurgien, 
+  plotdataset[1, ] <- list("Junior", percentage_xpchir(dataset, "Junior"))
+  plotdataset[2, ] <- list("Senior", percentage_xpchir(dataset, "Senior"))
+  plotdataset[3, ] <- list("Expérimenté", percentage_xpchir(dataset, "Expérimenté"))
+  
+  plotdataset$Chirurgien <- factor(
+    plotdataset$Chirurgien, 
     levels=c(
       "Junior", 
       "Senior",
@@ -170,44 +243,46 @@ generate_plot_patients_repartition_by_xp_chir <- function(dataset) {
     )
   )
   
-  plot = dataset %>%
-    ggplot(      aes(x=Chirurgien)
-    ) +
-    geom_bar(
-      stat='count',
-      orientation="x", 
-      alpha=0.9,
-      colour="white",
+  plot = plotdataset %>%
+    ggplot( aes(x = Chirurgien, y = Percentage)) +
+    geom_col(
+      alpha=0.6,
       fill=color_generic_stats(),
       width=0.7
     ) +
-    scale_y_continuous(labels=scales::percent) +
+    geom_text(aes(label = paste(Percentage, "%")), vjust = -0.3, colour = "black") +
     labs (
-      title= "Répartition des patients en fonction de l'expérience du Chirurgien",
+      title= "Répartition des patients en fonction de l'expérience du chirurgien",
       y="Nombre de cas",
-      x="Chirurgien"
+      x="Expérience du Chirurgien"
     ) +
     default_theming(
       y_title_angle = 90,
       title_size = 14,
-      legend_title_size = 12
+      legend_title_size = 12,
+      y_text_size = 0,
+      y_title_size = 0
     )
-  
   return(plot)
   
 }
 
 
 generate_plot_patients_repartition_by_matta <- function(dataset) {
+  N <- 1e4
+  plotdataset <- data.frame(
+    Matta=character(),
+    Percentage=integer(),
+    stringsAsFactors=FALSE
+  )
   
-  dataset$MattaFull <- as.character(dataset$Matta)
-  dataset$MattaFull[dataset$Matta == "1"] <- "Anatomique"
-  dataset$MattaFull[dataset$Matta == "2"] <- "Satisfaisant"
-  dataset$MattaFull[dataset$Matta == "3"] <- "Non Satisfaisant"
+  plotdataset[1, ] <- list("Anatomique", percentage_matta(dataset, "1"))
+  plotdataset[2, ] <- list("Satisfaisant", percentage_matta(dataset, "2"))
+  plotdataset[3, ] <- list("Non Satisfaisant", percentage_matta(dataset, "3"))
   
   #defining order for "Chirurgien"  (/!\ if orientation=x order needs to be reversed)
-  dataset$MattaFull <- factor(
-    dataset$MattaFull, 
+  plotdataset$Matta <- factor(
+    plotdataset$Matta, 
     levels=c(
       "Anatomique", 
       "Satisfaisant",
@@ -215,30 +290,26 @@ generate_plot_patients_repartition_by_matta <- function(dataset) {
     )
   )
   
-  plot = dataset %>%
-    ggplot(      aes(x=MattaFull)
-    ) +
-    geom_bar(
-      # position=
-      stat='count', 
-      orientation="x", 
-      alpha=0.9,
-      colour="white",
+  plot=plotdataset %>%
+    ggplot( aes(x = Matta, y = Percentage)) +
+    geom_col(
+      alpha=0.6,
       fill=color_generic_stats(),
       width=0.7
     ) +
-    # scale_y_continuous(labels=scales::percent) +
+    geom_text(aes(label = paste(Percentage, "%")), vjust = -0.3, colour = "black") +
     labs (
-      title= "Répartition des patients en fonction du score de Matta",
+      title= "Répartition des patients en fonction de l'expérience du chirurgien",
       y="Nombre de cas",
-      x="Matta"
+      x="Expérience du Chirurgien"
     ) +
     default_theming(
       y_title_angle = 90,
       title_size = 14,
-      legend_title_size = 12
+      legend_title_size = 12,
+      y_text_size = 0,
+      y_title_size = 0
     )
-  
   return(plot)
   
 }
